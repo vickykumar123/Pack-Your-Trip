c12e7d447cc50f415ff696ca65c5b1d37fa5725d
vivvk <vickykumarsunilja@gmail.com>
2023-08-02 19:22:37 +0530

    Update the LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Logo from "./Components/Logo";
 import Form from "./Components/Form";
 import PackageList from "./Components/PackageLists";
@@ -11,11 +11,23 @@ import Stats from "./Components/Stats";
 export default function App() {
   //Lifting State up- Is the concenpt to move to state nearest parent, So it can be used by both the child components;
 
-  const [items, setItems] = useState([]);
+  // const [items, setItems] = useState([]);
+  const [items, setItems] = useState(function () {
+    const storedItems = localStorage.getItem("items");
+    return storedItems ? JSON.parse(storedItems) : [];
+  });
   // const numItems = items.length;
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("items", JSON.stringify(items));
+    },
+    [items]
+  );
+
   function handleDelete(id) {
     console.log(id);
     setItems((items) => items.filter((item) => item.id !== id));
